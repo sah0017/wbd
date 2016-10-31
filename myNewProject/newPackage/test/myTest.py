a36229e41e93591bd55d9c6bdaef4d4e7110484e
susanha <susanha@faulkner.edu>
2016-10-31 11:34:05 -0500

    Initial commit

---
+++ myNewProject/newPackage/test/myTest.py
@@ -0,0 +1,17 @@
+'''
+Created on Oct 31, 2016
+
+@author: susanha
+'''
+
+class myTest(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
